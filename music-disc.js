aed6b5fab2c67c74f186df1364102383b57d04da
Hiya123C <christinelee263@gmail.com>
2025-12-15 07:33:34 +0000

    fixed + added sound effect to music disc

--- music-disc.js
+++ music-disc.js
@@ -1,18 +1,29 @@
-const discs = document.querySelectorAll(".disc");
-const images = document.querySelectorAll(".disc-img");
-let index = 0;
+const sound = document.getElementById("discSound");
 
-discs[0].style.visibility = "visibile";
+function playSound() {
+    sound.currentTime = 0;
+    sound.play();
+}
+
+const discs = document.querySelectorAll(".disc");
+let index = 0;
+let isAnimating = false;
+
+discs[0].style.visibility = "visible";
 discs[0].style.opacity = "1";
 
 function cycleDisc() {
-    const outgoing = discs[index];
+    if (isAnimating) return;
+    isAnimating = true;
 
+    playSound();
+
+    const outgoing = discs[index];
     index = (index + 1) % discs.length;
     const incoming = discs[index];
 
     discs.forEach(d => {
-        d.classList.remove("curve-in", "curve-out", "hidden");
+        d.classList.remove("curve-in", "curve-out");
     });
 
     outgoing.classList.add("curve-out");
@@ -20,14 +31,15 @@ function cycleDisc() {
     setTimeout(() => {
         outgoing.style.visibility = "hidden";
         outgoing.style.opacity = "0";
+
+        incoming.style.visibility = "visible";
+        incoming.style.opacity = "1";
+        incoming.classList.add("curve-in");
     }, 700);
 
-    incoming.style.visibility = "visible";
-    incoming.style.opacity = "1";
-
     setTimeout(() => {
-        incoming.classList.add("curve-in");
-    }, 30);
+        isAnimating = false;
+    }, 750);
 }
 
 document.querySelectorAll(".disc-img").forEach(disc => {
